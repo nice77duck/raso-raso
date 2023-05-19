dc20f4079899b7be4ce65671454250fac7f7d7ca
nice77duck <niceduck77@gmail.com>
2023-05-19 17:23:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,47 @@
-import telegram
-from telegram.ext import CommandHandler, Updater
-import os
+import logging
+from telegram import InlineQueryResultDocument, InlineQueryResultVideo, InlineQueryResultAudio
+from telegram.ext import Updater, InlineQueryHandler, MessageHandler, Filters
+import pymongo
 
-# Define the token for your bot obtained from BotFather
-TOKEN = "YOUR_BOT_TOKEN"
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Define a handler function for the /start command
-def start(update, context):
-    update.message.reply_text("Hi! I'm your file search bot. Use /search to look for files in this channel.")
-
-# Define a handler function for the /help command
-def help(update, context):
-    update.message.reply_text("To search for a file, use /search followed by the name of the file. For example: /search hello.txt")
-
-# Define a handler function for the /search command
-def search_files(update, context):
-    # Get the text after the /search command
-    query = ' '.join(context.args)
-
-    # Search for files in the private channel directory
-    files = []
-    for file in os.listdir("private_channel_directory"):
-        if query in file:
-            files.append(file)
-
-    # Forward the matching files to the user chat
-    if len(files) > 0:
-        for file in files:
-            context.bot.send_document(update.message.chat_id, document=open(os.path.join("private_channel_directory", file), 'rb'))
-    else:
-        update.message.reply_text("No matching files found.")
-
-# Create an instance of the bot and add handlers for the commands
-bot = telegram.Bot(TOKEN)
-updater = Updater(TOKEN, use_context=True)
+# Initialize the Telegram bot
+updater = Updater(token='YOUR_TELEGRAM_BOT_TOKEN', use_context=True)
 dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("help", help))
-dispatcher.add_handler(CommandHandler("search", search_files))
 
-# Start the bot
+# Set up the database connection
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['your_database_name']
+files_collection = db['files']
+
+def handle_files(update, context):
+    if update.message.document or update.message.video or update.message.audio:
+        file_info = update.message.document or update.message.video or update.message.audio
+        caption = file_info.caption or ''
+        file_data = {
+            'file_id': file_info.file_id,
+            'file_type': file_info.mime_type,
+            'caption': caption
+        }
+        files_collection.insert_one(file_data)
+
+def inline_search(update, context):
+    query = update.inline_query.query
+    search_results = files_collection.find({'$text': {'$search': query}})
+    results = []
+    for result in search_results:
+        if result['file_type'].startswith('video'):
+            results.append(InlineQueryResultVideo(id=result['file_id'], video_url=result['file_id']))
+        elif result['file_type'].startswith('audio'):
+            results.append(InlineQueryResultAudio(id=result['file_id'], audio_url=result['file_id']))
+        else:
+            results.append(InlineQueryResultDocument(id=result['file_id'], document_url=result['file_id']))
+    update.inline_query.answer(results)
+
+file_handler = MessageHandler(Filters.document | Filters.video | Filters.audio, handle_files)
+inline_search_handler = InlineQueryHandler(inline_search)
+
+dispatcher.add_handler(file_handler)
+dispatcher.add_handler(inline_search_handler)
+
 updater.start_polling()
-updater.idle()
