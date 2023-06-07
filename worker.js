aab64abf27189e269115e01b799351bcf4a20b71
nice77duck <niceduck77@gmail.com>
2023-06-08 05:59:20 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,16 @@
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  if (url.hostname in ORIGINS) {
+    const target = ORIGINS[url.hostname]
+    url.hostname = target
+    return fetch(url.toString(), request)
+  }
+  return fetch(request)
+}
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+const ORIGINS = {
+  'proxysite.niceduck.workers.dev': '159.223.132.128:8080'
+}   
